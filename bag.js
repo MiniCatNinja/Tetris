a00ca960689930c6469cf866490e45d5ce225732
Leo <leoquan09@gmail.com>
2026-04-08 20:59:04 -0700

    made grid and css appear on screen

--- bag.js
+++ bag.js
@@ -1,12 +1,17 @@
 let bag = [1, 2, 3, 4, 5, 6, 7];
 
 function shuffleBag() {
-    let bag = [1, 2, 3, 4, 5, 6, 7];
-    for (let i = bag.length - 1; i > 0; i--) {
-  let j = Math.floor(Math.random() * (i + 1));
-  [bag[i], bag[j]] = [bag[j], bag[i]]; // swap
-    }
+  let bag = [1, 2, 3, 4, 5, 6, 7];
+
+  for (let i = bag.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [bag[i], bag[j]] = [bag[j], bag[i]]; // swap
+  }
 }
+
+shuffleBag();
+console.log(bag);
+
 //1 = I Block
 //2 = O Block
 //3 = T Block
@@ -14,6 +19,3 @@ function shuffleBag() {
 //5 = Z Block
 //6 = J Block
 //7 = L Block
-
-shuffleBag();
-console.log(bag);
---
+++ grid.js
@@ -0,0 +1,6 @@
+const game = document.getElementById('content');
+
+for (let i = 0; i < 200; i++) {
+    const cell = document.createElement('div');
+    game.appendChild(cell);
+}
